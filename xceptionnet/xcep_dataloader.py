4072d1e9de5f96902bc2cc096a2ff9ed9d60f06d
mlzr <alfkwwkd@gmail.com>
2022-08-18 23:04:30 +0900

    FEAT: update dataloader and main

--- xceptionnet/xcep_dataloader.py
+++ xceptionnet/xcep_dataloader.py
@@ -2,17 +2,30 @@ import torch
 import torch.nn as nn
 import torchvision
 import numpy as np
-import glob
 from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from torchvision import transforms
 
-all_train = glob.glob()
-all_val = 
-all_test = 
+
+class CustomDataset(Dataset):
+    # Put cat and dog images together into CustomDataset
+    def __init__(self, file_list, transform):
+        # Labeling
+        self.file_list = file_list
+        self.transform = transform
+        for i in file_list:
+            if 'cat' in i.split('/')[-1]:
+                self.label = 0
+            elif 'dog' in i.split('/')[-1]:
+                self.label = 1
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+
+
 
 
 
 
 
 if __name__ == "__main__":
-    a=1
+    a=CustomDataset()
--- xceptionnet/xcep_main.py
+++ xceptionnet/xcep_main.py
@@ -1,4 +1,41 @@
 
+from torchvision import transforms
+import glob
+from xcep_dataloader import CustomDataset
+
+
+# All train/validation/test file names regardless of cat of dog -> need to modularization
+all_train_jpg_list = glob.glob("/home/mlzr/Classification/xceptionnet/cats_and_dogs_small/train/*/*.jpg")
+all_val_jpg_list = glob.glob("/home/mlzr/Classification/xceptionnet/cats_and_dogs_small/validation/*/*.jpg")
+all_test_jpg_list = glob.glob("/home/mlzr/Classification/xceptionnet/cats_and_dogs_small/test/*/*.jpg")
+
+# Split cat and dog by file names -> need to modularization
+train_cat_jpg = [i for i in all_train_jpg_list if 'cat' in i.split('/')[-1]]
+train_dog_jpg = [i for i in all_train_jpg_list if 'dog' in i.split('/')[-1]]
+val_cat_jpg = [i for i in all_val_jpg_list if 'cat' in i.split('/')[-1]]
+val_dog_jpg = [i for i in all_val_jpg_list if 'dog' in i.split('/')[-1]]
+test_cat_jpg = [i for i in all_test_jpg_list if 'cat' in i.split('/')[-1]]
+test_dog_jpg = [i for i in all_test_jpg_list if 'dog' in i.split('/')[-1]]
+
+# Concat cat and dog images by train/validation/test
+train_catdog_jpg = train_cat_jpg + train_dog_jpg
+val_catdog_jpg = val_cat_jpg + val_dog_jpg
+test_catdog_jpg = test_cat_jpg + test_dog_jpg
+
+
+# Define Image Transform function -> need to modularization
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(299),
+    transforms.ToTensor(),])
+val_transform = transforms.Compose([
+    transforms.Resize(299),
+    transforms.ToTensor(),])
+test_transform = transforms.Compose([
+    transforms.Resize(299),
+    transforms.ToTensor(),])
+
+
 
 
 
@@ -7,4 +44,21 @@
 
 
 if __name__ == "__main__":
+    # print("Train list:", all_train_jpg_list[ :4], all_train_jpg_list[-4: ])
+    # print("Validation list:", all_val_jpg_list[ :4], all_val_jpg_list[-4: ])
+    # print("Test list:", all_test_jpg_list[ :4], all_test_jpg_list[-4: ])
+
+    # print("Train dataset length:", len(all_train_jpg_list))
+    # print("Validation dataset length:", len(all_val_jpg_list))
+    # print("Test dataset length:", len(all_test_jpg_list))
     
+    # print("Train cat file name list:", train_cat_jpg[ :4], train_cat_jpg[-4: ])
+    # print("Train dog file name list:", train_dog_jpg[ :4], train_dog_jpg[-4: ])
+    # print("Validation cat file name list:", train_cat_jpg[ :4], train_cat_jpg[-4: ])
+    # print("Validation dog file name list:", val_dog_jpg[ :4], val_dog_jpg[-4: ])
+    # print("Test cat file name list:", test_cat_jpg[ :4], test_cat_jpg[-4: ])
+    # print("Test dog file name list:", test_dog_jpg[ :4], test_dog_jpg[-4: ])
+
+    # print(len(train_cat_jpg), len(train_catdog_jpg))
+    haha = ['hehe/he', 'hoho/ho']
+    a = CustomDataset(haha)
